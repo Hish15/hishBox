efaada5fccf4afb9f0d5113d10af0015e065ca60
Hector PHARAM <pharamhector@gmail.com>
2024-12-09 23:28:26 +0100

    Using SFML library to read sound

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ extern "C"{
 }
 #include <nfc_reader.hpp>
 #include <nfc_frame.hpp>
+#include <media_player.hpp>
 
 #include <spdlog/spdlog.h>
 
@@ -23,90 +24,90 @@ extern "C"{
 #include <vector>
 /*
 
-+-------------------------+----------------------------------------------------------------
-| D1                      | Record header (MB = ME = 1, CF = 0, SR = 1, IL = 0, TNF = 0x1)
-+-------------------------+----------------------------------------------------------------
-| 01                      | Type Length (1 byte)
-+-------------------------+----------------------------------------------------------------
-| 0B                      | Payload Length (11 bytes)
-+-------------------------+----------------------------------------------------------------
-| 55                      | Type Name ("U")
-+-------------------------+----------------------------------------------------------------
-| 02 67 6F 6F 67 6C 65 2E | Payload: Identifier code = 2 (prefix "https://www."),
-| 63 6F 6D                |          truncated URI = "google.com"
-+-------------------------+----------------------------------------------------------------
-*/
+   +-------------------------+----------------------------------------------------------------
+   | D1                      | Record header (MB = ME = 1, CF = 0, SR = 1, IL = 0, TNF = 0x1)
+   +-------------------------+----------------------------------------------------------------
+   | 01                      | Type Length (1 byte)
+   +-------------------------+----------------------------------------------------------------
+   | 0B                      | Payload Length (11 bytes)
+   +-------------------------+----------------------------------------------------------------
+   | 55                      | Type Name ("U")
+   +-------------------------+----------------------------------------------------------------
+   | 02 67 6F 6F 67 6C 65 2E | Payload: Identifier code = 2 (prefix "https://www."),
+   | 63 6F 6D                |          truncated URI = "google.com"
+   +-------------------------+----------------------------------------------------------------
+   */
 
 
 std::string get_youtube_id(std::string url)
 {
-    std::smatch m;
-    if(std::regex_search(url, m, std::regex("youtu\\.?be")) == false)
-    {
-        return "";
-    }
-    if(std::regex_search(url, m, std::regex("v=([a-zA-Z0-9]+)")))
-    {
-        return m[1];
-    }
-    else if(std::regex_search(url, m, std::regex("//.[^/]+/([a-zA-Z]+)"))) 
-    {
-        return m[1];
-    }
-    return "";
+	std::smatch m;
+	if(std::regex_search(url, m, std::regex("youtu\\.?be")) == false)
+	{
+		return "";
+	}
+	if(std::regex_search(url, m, std::regex("v=([a-zA-Z0-9]+)")))
+	{
+		return m[1];
+	}
+	else if(std::regex_search(url, m, std::regex("//.[^/]+/([a-zA-Z]+)"))) 
+	{
+		return m[1];
+	}
+	return "";
 }
 
 std::string get_matching_path(std::string_view directory, std::string_view pattern)
 {
-    // directory_iterator can be iterated using a range-for loop
-    for (auto const& dir_entry : std::filesystem::directory_iterator{directory}) 
-    {
-        const bool is_file = std::filesystem::is_regular_file(dir_entry);
-        if(is_file == false)
-        {
-            continue;
-        }
-        const std::string tested_file = dir_entry.path().string();
-        if(dir_entry.path().string().contains(pattern))
-            return tested_file;
-    }
-    return "";
-    
+	// directory_iterator can be iterated using a range-for loop
+	for (auto const& dir_entry : std::filesystem::directory_iterator{directory}) 
+	{
+		const bool is_file = std::filesystem::is_regular_file(dir_entry);
+		if(is_file == false)
+		{
+			continue;
+		}
+		const std::string tested_file = dir_entry.path().string();
+		if(dir_entry.path().string().contains(pattern))
+			return tested_file;
+	}
+	return "";
+
 }
 
 int main(int argc, char** argv) {
-    std::expected<NfcReader, std::string> try_reader = NfcReader::try_init();
-    if(try_reader.has_value() == false)
-    {
-	spdlog::error(try_reader.error());
-	return -1;
-    }
-    NfcReader& reader = try_reader.value();
-    printf("Waiting for RFID/NFC card...\r\n");
-    printf("Reading blocks...\r\n");
-    std::vector<uint8_t> raw_frame = reader.read_card();
+	std::expected<NfcReader, std::string> try_reader = NfcReader::try_init();
+	if(try_reader.has_value() == false)
+	{
+		spdlog::error(try_reader.error());
+		return -1;
+	}
+	NfcReader& reader = try_reader.value();
+	printf("Waiting for RFID/NFC card...\r\n");
+	printf("Reading blocks...\r\n");
+	std::vector<uint8_t> raw_frame = reader.read_card();
 
-    auto payload = NfcFrame::get_payload(raw_frame);
+	auto payload = NfcFrame::get_payload(raw_frame);
 
-    std::string url(payload.begin(), payload.end());
-    const auto id = get_youtube_id(url);
-    if(id.empty() == false)
-    {
-	    const auto matching_path = get_matching_path("youtube",id);
-	    if(matching_path.empty())
-	    {
-		    std::string download_cmd = fmt::format("docker run --rm -it -v \"/home/hish/hishBox/youtube/:/downloads:rw\" ghcr.io/jauderho/yt-dlp:latest {} --extract-audio --audio-format wav",url );
+	std::string url(payload.begin(), payload.end());
+	const auto id = get_youtube_id(url);
+    MediaPlayer player;
+	if(id.empty() == false)
+	{
+		const auto matching_path = get_matching_path("youtube",id);
+		if(matching_path.empty())
+		{
+			std::string download_cmd = fmt::format("docker run --rm -it -v \"/home/hish/hishBox/youtube/:/downloads:rw\" ghcr.io/jauderho/yt-dlp:latest {} --extract-audio --audio-format wav",url );
 
-		    std::system(download_cmd.c_str());
-	    }
-	    const auto matching_path2 = get_matching_path("youtube", id);
-	    const auto run_cmd = fmt::format("mplayer \"{}\"", matching_path2);
-	    spdlog::info(run_cmd);
-	    std::system(run_cmd.c_str());
-    }
-    else
-    {
-	spdlog::error("The URL is not handled yet");
-    }
-    return 0;
+			std::system(download_cmd.c_str());
+		}
+		const auto matching_path2 = get_matching_path("youtube", id);
+		spdlog::info(matching_path2);
+        player.start_song(matching_path2);
+	}
+	else
+	{
+		spdlog::error("The URL is not handled yet");
+	}
+	return 0;
 }
