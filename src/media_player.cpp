adc64b1a19d5b9c77bb54c49aed8d8c3ed1cd530
Hector PHARAM <pharamhector@gmail.com>
2024-12-11 21:31:27 +0100

    MediaPlayer:moved function to .cpp file

---
+++ src/media_player.cpp
@@ -0,0 +1,39 @@
+#include "media_player.hpp"
+
+#include <SFML/Audio.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
+
+void MediaPlayer::start_song(std::string path_to_file)
+{
+
+
+    // Play the sound
+    //m_play_thread.request_stop();
+    //m_play_thread.join(); //Wait for the thread to stop
+    m_play_thread = std::jthread([path_to_file](std::stop_token stoken)
+    {
+        // Declare a new sound buffer
+        sf::SoundBuffer buffer;
+
+        // Load it from a file
+        if (!buffer.loadFromFile(path_to_file))
+        {
+            // error...
+        }
+
+        // Create a sound source and bind it to the buffer
+        sf::Sound sound;
+        sound.setBuffer(buffer);
+        sound.play();
+        // Loop while the sound is playing
+        while (sound.getStatus() == sf::Sound::Status::Playing)
+        {
+            if(stoken.stop_requested())
+            {
+                break;
+            }
+            // Leave some CPU time for other processes
+            sf::sleep(sf::milliseconds(100));
+        }
+    });
+}
--- src/media_player.hpp
+++ src/media_player.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <SFML/Audio.hpp>
-#include <SFML/Audio/SoundBuffer.hpp>
 #include <thread>
 #include <iostream>
 
@@ -9,36 +7,6 @@
 class MediaPlayer
 {
     std::jthread m_play_thread;
-    public:
-    void start_song(std::string path_to_file)
-    {
-
-        // Declare a new sound buffer
-sf::SoundBuffer buffer;
- 
-// Load it from a file
-if (!buffer.loadFromFile(path_to_file))
-{
-    // error...
-}
- 
-// Create a sound source and bind it to the buffer
-sf::Sound sound;
-sound.setBuffer(buffer);
- 
-// Play the sound
-sound.play();
-
-        // Loop while the sound is playing
-        while (sound.getStatus() == sf::Sound::Status::Playing)
-        {
-            // Leave some CPU time for other processes
-            sf::sleep(sf::milliseconds(100));
-
-            // Display the playing position
-            std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        " << std::flush;
-        }
-
-        std::cout << '\n' << std::endl;
-    }
+public:
+    void start_song(std::string path_to_file);
 };
