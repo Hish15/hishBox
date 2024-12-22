00bfeb0d563f1c0585ebfeb23ba78d7733d847b3
Hector PHARAM <pharamhector@gmail.com>
2024-12-22 22:41:20 +0100

    Adding buttons handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@ extern "C"{
 #include <time.h>
 #include "pn532.h"
 #include "pn532_rpi.h"
-
-
+#include <wiringPi.h>
 }
+
 #include <nfc_reader.hpp>
 #include <nfc_frame.hpp>
 #include <media_player.hpp>
@@ -76,9 +76,54 @@ std::string get_matching_path(std::string_view directory, std::string_view patte
 
 }
 
+#define PIN_BUTTON 18
+
 MediaPlayer media_player;
 
+#include <functional>
+struct InitGpio
+{
+	int pin;
+	int mode;
+	enum class pull {off = 0, up, down};
+	pull pull;
+	std::function<void(void)> callback;
+};
+
+class GpioHandler
+{
+public:
+	GpioHandler(std::initializer_list<InitGpio> list_gpio)
+	{
+		for(const auto& gpio : list_gpio)
+		{
+			wiringPiSetupGpio();
+			pinMode(gpio.pin, gpio.mode);
+			if (gpio.mode == INPUT)
+			{
+				switch(gpio.pull)
+				{
+					case InitGpio::pull::off:
+						pullUpDnControl(gpio.pin, PUD_OFF);
+						break;
+					case InitGpio::pull::up:
+						pullUpDnControl(gpio.pin, PUD_UP);
+						break;
+					case InitGpio::pull::down:
+						pullUpDnControl(gpio.pin, PUD_DOWN);
+						break;
+				}
+			}
+			wiringPiISR(gpio.pin, INT_EDGE_FALLING, gpio.callback.target<void(void)>());
+		}
+	}
+};
+
 int main(int argc, char** argv) {
+
+	GpioHandler gpio_handler({
+		{PIN_BUTTON, INPUT, InitGpio::pull::up, [](){media_player.next();}}
+	});
 
 	std::vector<std::string> list_songs;
 	list_songs.push_back("./youtube/Nicki Minaj - Anaconda [LDZX4ooRsWs].wav");
@@ -86,6 +131,16 @@ int main(int argc, char** argv) {
     //TEST
     media_player.load_list(list_songs);
     using namespace std::chrono_literals;
+
+	while(1)
+	{
+		if(digitalRead(PIN_BUTTON) == LOW)
+		{
+			media_player.next();
+			std::this_thread::sleep_for(1s);
+		}
+	}
+	media_player.play();
     
 	std::this_thread::sleep_for(10s);
     media_player.pause();
